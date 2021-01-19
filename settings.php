sanjaydatasciencedojo/o365-moodle

Add configuration setting to control if fall back to group creation.

local/o365/settings.php
@@ -250,6 +250,10 @@ if ($hassiteconfig) {
         $desc = new lang_string('settings_sign_off_integration_details', 'local_o365', $CFG->wwwroot);
         $settings->add(new \admin_setting_configcheckbox('local_o365/single_sign_off', $label, $desc, '0'));
 
+        $label = new lang_strign('settings_group_creation_fallback', 'local_o365');
+        $desc = new lang_string('settings_group_creation_fallback_details', 'local_o365');
+        $settings->add(new \admin_setting_configcheckbox('local_o365/group_creation_fallback', $label, $desc, '1'));
+
         $label = new lang_string('settings_o365china', 'local_o365');
         $desc = new lang_string('settings_o365china_details', 'local_o365');
         $settings->add(new \admin_setting_configcheckbox('local_o365/chineseapi', $label, $desc, '0'));

microsoft/moodle-local_o365

settings.php
@@ -250,6 +250,10 @@ if ($hassiteconfig) {
         $desc = new lang_string('settings_sign_off_integration_details', 'local_o365', $CFG->wwwroot);
         $settings->add(new \admin_setting_configcheckbox('local_o365/single_sign_off', $label, $desc, '0'));
 
+        $label = new lang_string('settings_group_creation_fallback', 'local_o365');
+        $desc = new lang_string('settings_group_creation_fallback_details', 'local_o365');
+        $settings->add(new \admin_setting_configcheckbox('local_o365/group_creation_fallback', $label, $desc, '1'));
+
         $label = new lang_string('settings_o365china', 'local_o365');
         $desc = new lang_string('settings_o365china_details', 'local_o365');
         $settings->add(new \admin_setting_configcheckbox('local_o365/chineseapi', $label, $desc, '0'));