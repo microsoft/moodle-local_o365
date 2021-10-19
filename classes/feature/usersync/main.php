Olivierwenger/o365-moodle

Prevent processing the same user multiple times in user sync

local/o365/classes/feature/usersync/main.php
@@ -949,6 +949,8 @@ class main {
 
         $existingusers = $existingusers + $linkedexistingusers;
 
+        $processedusers = [];
+
         foreach ($aadusers as $user) {
             $this->mtrace(' ');
 
@@ -969,6 +971,13 @@ class main {
             $user['convertedupn'] = $user['upnlower'];
             if (stripos($user['userPrincipalName'], '#EXT#') !== false) {
                 $user['convertedupn'] = strtolower($user['mail']);
+            }
+
+            if (in_array($user['convertedupn'], $processedusers)) {
+                $this->mtrace('User already process; skipping...');
+                continue;
+            } else {
+                $processedusers[] = $user['convertedupn'];
             }
 
             if (!isset($existingusers[$user['upnlower']]) && !isset($existingusers[$user['upnsplit0']]) &&

microsoft/moodle-local_o365

classes/feature/usersync/main.php
@@ -949,6 +949,8 @@ class main {
 
         $existingusers = $existingusers + $linkedexistingusers;
 
+        $processedusers = [];
+
         foreach ($aadusers as $user) {
             $this->mtrace(' ');
 
@@ -969,6 +971,13 @@ class main {
             $user['convertedupn'] = $user['upnlower'];
             if (stripos($user['userPrincipalName'], '#EXT#') !== false) {
                 $user['convertedupn'] = strtolower($user['mail']);
+            }
+
+            if (in_array($user['convertedupn'], $processedusers)) {
+                $this->mtrace('User already processed; skipping...');
+                continue;
+            } else {
+                $processedusers[] = $user['convertedupn'];
             }
 
             if (!isset($existingusers[$user['upnlower']]) && !isset($existingusers[$user['upnsplit0']]) &&