catalyst-ca/o365-moodle

bug fix in creating connection records for new accounts in user sync task run

local/o365/classes/feature/usersync/main.php
@@ -854,7 +854,7 @@ class main {
                 'type' => 'user',
                 'subtype' => '',
                 'objectid' => $userobjectid,
-                'o365name' => $aaddata['userPrincipalName'],
+                'o365name' => $aaddata['useridentifier'],
                 'moodleid' => $newuser->id,
                 'tenant' => '',
                 'timecreated' => $now,

microsoft/moodle-local_o365

classes/feature/usersync/main.php
@@ -913,7 +913,7 @@ class main {
                 'type' => 'user',
                 'subtype' => '',
                 'objectid' => $userobjectid,
-                'o365name' => $entraiduserdata['userPrincipalName'],
+                'o365name' => $entraiduserdata['useridentifier'],
                 'moodleid' => $newuser->id,
                 'tenant' => '',
                 'timecreated' => $now,