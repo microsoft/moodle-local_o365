6afd97b1a4534ee971a5e53d13bb6a1f068c975c
Patryk Mroczko <patryk.mroczko@enovation.ie>
2025-12-23 12:11:02 +0100

    Update plugin versions for 5.0.4 release

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025040815;
+$plugin->version = 2025040820;
 $plugin->requires = 2025040800;
-$plugin->release = '5.0.3';
+$plugin->release = '5.0.4';
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'auth_oidc' => 2025040810,
+    'auth_oidc' => 2025040815,
 ];
